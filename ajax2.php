1d33fb2a42925385007797ff25b7afbcf4d9e3ba
gaojia <535702682@qq.com>
2016-11-01 20:51:51 +0800

    测试跨协议

--- ajax2.php
+++ ajax2.php
@@ -1,9 +1,9 @@
 <?php
 	$data=$_POST["data"];
 	$text=explode("#",$data);
-	header("http://op.juhe.cn/onebox/football/combat?dtype=&".$text[0]."&".$text[1]."&".$text[2]);
+	header("//op.juhe.cn/onebox/football/combat?dtype=&".$text[0]."&".$text[1]."&".$text[2]);
 	header("Access-Control-Allow-Method:GET");
-	$url="http://op.juhe.cn/onebox/football/combat?dtype=&".$text[0]."&".$text[1]."&".$text[2];
+	$url="//op.juhe.cn/onebox/football/combat?dtype=&".$text[0]."&".$text[1]."&".$text[2];
 	$html=file_get_contents($url);
 	echo $html;
 ?>
